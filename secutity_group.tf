66582227bc6a07b0b919f060fe50aa3dcff966c6
knpatel5900 <59032477+knpatel5900@users.noreply.github.com>
2020-08-16 01:29:46 -0400

    Create secutity_group.tf

---
+++ secutity_group.tf
@@ -0,0 +1,28 @@
+resource "aws_security_group" "terraform_sg" {
+  vpc_id       = aws_vpc.Terraform_VPC.id
+  name         = "terrform_sg"
+  description  = "My Terraform VPC Security Group"
+ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+  }
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+   Name = "terraform_sg"
+   Description = "My VPC Security Group"
+}
+}
